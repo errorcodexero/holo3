174a677e2751b1d2e0c116699e4cc7495fa32a76
Steve Tarr <stevet@RHUBARB>
2013-01-14 18:32:09 -0800

    created RobotDrive3 for holonomic drive base

---
+++ Subsystems/RobotDrive3.h
@@ -0,0 +1,31 @@
+#ifndef ROBOTDRIVE3_H_
+#define ROBOTDRIVE3_H_
+
+#include "ErrorBase.h"
+#include <stdlib.h>
+#include <vxWorks.h>
+#include "RobotDrive.h"
+
+class SpeedController;
+
+// RobotDrive extensions for 3-wheel holonomic drive
+
+class RobotDrive3 : public RobotDrive
+{
+public:
+    RobotDrive3(UINT32 frontMotorChannel, UINT32 rearLeftMotorChannel,
+		UINT32 rearRightMotorChannel);
+    RobotDrive3(SpeedController *frontMotor, SpeedController *rearLeftMotor,
+		SpeedController *rearRightMotor);
+    RobotDrive3(SpeedController &frontMotor, SpeedController &rearLeftMotor,
+		SpeedController &rearRightMotor);
+
+    void HolonomicDrive_Cartesian(float x, float y, float rotation,
+    				  float gyroAngle = 0.0);
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(RobotDrive3);
+};
+
+#endif
+
