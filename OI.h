d73ab1c66af30f726cfb36550e9fd0d083d4a729
U-PC-RHO\Xudomus <Xudomus@PC-RHO.(none)>
2013-02-16 10:12:31 -0800

    Working Camera-Align, as well as most buttons for Gamepad

--- OI.h
+++ OI.h
@@ -6,16 +6,31 @@
 
 #include "WPILib.h"
 
+
 class OI {
 private:
 	Joystick* stick;
-	JoystickButton* button2;
-	
+	JoystickButton* button1; //On Gamepad, is mapped to "A"
+	JoystickButton* button2; //On Gamepad, is mapped to "B"
+	JoystickButton* button3; //On Gamepad, is mapped to "X"
+	JoystickButton* button4; //On Gamepad, is mapped to "Y"
+	JoystickButton* button5; //On Gamepad, is mapped to "LeftBumper"
+	JoystickButton* button6; //On Gamepad, is mapped to "RightBumper"
+	JoystickButton* button7; //On Gamepad, is mapped to "Back"
+	JoystickButton* button8; //On Gamepad, is mapped to "Start"
 public:
 	OI();
 	Joystick* getStick();
 	float GetDriverThrottle()	{ return stick->GetThrottle(); }
+	bool GetDriverButton1() {return button1->Get();}
 	bool GetDriverButton2() {return button2->Get();}
+	bool GetDriverButton3() {return button3->Get();}
+	bool GetDriverButton4() {return button4->Get();}
+	bool GetDriverButton5() {return button5->Get();}
+	bool GetDriverButton6() {return button6->Get();}
+	bool GetDriverButton7() {return button7->Get();}
+	bool GetDriverButton8() {return button8->Get();}
+		
 };
 
 #endif
