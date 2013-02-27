c65a92ed99da065f91de224e8e6f55efe88ff73f
Steve Tarr <stevet@RHUBARB>
2013-02-26 23:07:28 -0800

    add OI controls

--- OI.cpp
+++ OI.cpp
@@ -6,41 +6,65 @@
 OI::OI() 
 {
     // Instantiate operator controls
-    m_stick = new Joystick(1);
-    m_gamepadButtonA = new JoystickButton(m_stick, 1);
-    m_gamepadButtonB = new JoystickButton(m_stick, 2);
-    m_gamepadButtonX = new JoystickButton(m_stick, 3);
-    m_gamepadButtonY = new JoystickButton(m_stick, 4);
-    m_gamepadLeftBumper = new JoystickButton(m_stick, 5);
-    m_gamepadRightBumper = new JoystickButton(m_stick, 6);
-    m_gamepadBack = new JoystickButton(m_stick, 7);
-    m_gamepadStart = new JoystickButton(m_stick, 8);
+    m_pDS = DriverStation::GetInstance();
+    m_pEIO = &m_pDS->GetEnhancedIO();
+    m_pLCD = DriverStationLCD::GetInstance();
+
+    m_pStick = new Joystick(1);
+    m_pGamepadButtonA     = new JoystickButton(m_pStick, 1);
+    m_pGamepadButtonB     = new JoystickButton(m_pStick, 2);
+    m_pGamepadButtonX     = new JoystickButton(m_pStick, 3);
+    m_pGamepadButtonY     = new JoystickButton(m_pStick, 4);
+    m_pGamepadLeftBumper  = new JoystickButton(m_pStick, 5);
+    m_pGamepadRightBumper = new JoystickButton(m_pStick, 6);
+    m_pGamepadBack        = new JoystickButton(m_pStick, 7);
+    m_pGamepadStart       = new JoystickButton(m_pStick, 8);
+
+    m_pClimber            = new DSAnalogInput(m_pEIO, 1);
+    m_pTip                = new DSAnalogInput(m_pEIO, 2);
+    m_pSpeedAdjust        = new DSAnalogInput(m_pEIO, 3);
+    m_pShooterPosition    = new DSAnalogInput(m_pEIO, 4);
+
+    m_pDump               = new DSDigitalInput(m_pEIO, 1);
+    m_pCameraLight        = new DSDigitalInput(m_pEIO, 2);
+    m_pCameraPosition     = new DSDigitalInput(m_pEIO, 3);
+    m_pQueryButton        = new DSDigitalInput(m_pEIO, 4);
+    m_pSpeedOverride      = new DSDigitalInput(m_pEIO, 5);
+    m_pLaunch             = new DSDigitalInput(m_pEIO, 6);
+    m_pKey                = new DSDigitalInput(m_pEIO, 7);
+
+    m_pReadyLED           = new DSDigitalOutput(m_pEIO, 8);
 }
 
 void OI::Initialize()
 {
     // Link controls to commands
-    m_gamepadButtonA->WhenPressed(new TargetCommand());
-    m_gamepadButtonB->WhenPressed(new SelectTarget("right"));
-    m_gamepadButtonX->WhenPressed(new SelectTarget("left"));
-    m_gamepadButtonY->WhenPressed(new SelectTarget("mid"));
-    m_gamepadLeftBumper->WhenPressed(new TimedDrive( 0.0, 0.0, -.35, 0.20 ));
-    m_gamepadRightBumper->WhenPressed(new TimedDrive( 0.0, 0.0, .35, 0.20 ));
-    m_gamepadBack->WhenPressed(new AimTrim(true));
-    m_gamepadStart->WhenPressed(new AimTrim(false));
+    m_pGamepadButtonA->WhenPressed(new TargetCommand());
+    m_pGamepadButtonB->WhenPressed(new SelectTarget("right"));
+    m_pGamepadButtonX->WhenPressed(new SelectTarget("left"));
+    m_pGamepadButtonY->WhenPressed(new SelectTarget("mid"));
+    m_pGamepadLeftBumper->WhenPressed(new TimedDrive( 0.0, 0.0, -.35, 0.20 ));
+    m_pGamepadRightBumper->WhenPressed(new TimedDrive( 0.0, 0.0, .35, 0.20 ));
+    m_pGamepadBack->WhenPressed(new AimTrim(true));
+    m_pGamepadStart->WhenPressed(new AimTrim(false));
 
-    m_rotateFwd = new Rotate(1);
-    SmartDashboard::PutData("Rotate Fwd", m_rotateFwd);
+    m_pRotateFwd = new Rotate(1);
+    SmartDashboard::PutData("Rotate Fwd", m_pRotateFwd);
 
-    m_rotateRev = new Rotate(-1);
-    SmartDashboard::PutData("Rotate Rev", m_rotateRev);
+    m_pRotateRev = new Rotate(-1);
+    SmartDashboard::PutData("Rotate Rev", m_pRotateRev);
 
-    m_shootShort = new ShootCommand( Shooter::kShort );
-    SmartDashboard::PutData("Shoot Short", m_shootShort);
+    m_pShootShort = new ShootCommand( Shooter::kShort );
+    SmartDashboard::PutData("Shoot Short", m_pShootShort);
 
-    m_shootMid = new ShootCommand( Shooter::kMid );
-    SmartDashboard::PutData("Shoot Mid", m_shootMid);
+    m_pShootMid = new ShootCommand( Shooter::kMid );
+    SmartDashboard::PutData("Shoot Mid", m_pShootMid);
 
-    m_shootLong = new ShootCommand( Shooter::kLong );
-    SmartDashboard::PutData("Shoot Long", m_shootLong);
+    m_pShootLong = new ShootCommand( Shooter::kLong );
+    SmartDashboard::PutData("Shoot Long", m_pShootLong);
+
+    // m_pBlinkyOn = new BlinkyOn();
+    // SmartDashboard::PutData("Blinky On", m_pBlinkyOn);
+    // m_pCameraLight->WhileHeld(m_pBlinkyOn);
 }
+
--- OI.h
+++ OI.h
@@ -8,31 +8,119 @@
 #include "Robot.h"
 
 
+class DSAnalogInput {
+private:
+    DriverStationEnhancedIO *m_pEIO;
+    UINT32 m_channel;
+
+public:
+    DSAnalogInput( DriverStationEnhancedIO *pEIO, UINT32 channel ) :
+	m_pEIO(pEIO),
+	m_channel(channel)
+    {
+	;
+    }
+
+    ~DSAnalogInput() {}
+
+    double GetAnalog() {
+	return m_pEIO->GetAnalogInRatio(m_channel); 
+    }
+
+    int GetDigital( int numPositions ) {
+	return (int)(GetAnalog() * numPositions) / numPositions;
+    }
+};
+
+class DSDigitalInput : Button {
+private:
+    DriverStationEnhancedIO *m_pEIO;
+    UINT32 m_channel;
+
+public:
+    DSDigitalInput( DriverStationEnhancedIO *pEIO, UINT32 channel ) :
+	m_pEIO(pEIO),
+	m_channel(channel)
+    {
+	m_pEIO->SetDigitalConfig(channel,
+	    DriverStationEnhancedIO::kInputPullUp);
+    }
+
+    ~DSDigitalInput() {}
+
+    bool Get() {
+	return m_pEIO->GetDigital(m_channel);
+    }
+};
+
+class DSDigitalOutput {
+private:
+    DriverStationEnhancedIO *m_pEIO;
+    UINT32 m_channel;
+
+public:
+    DSDigitalOutput( DriverStationEnhancedIO *pEIO, UINT32 channel ) :
+	m_pEIO(pEIO),
+	m_channel(channel)
+    {
+	m_pEIO->SetDigitalConfig(channel,
+	    DriverStationEnhancedIO::kOutput);
+    }
+
+    ~DSDigitalOutput() {}
+
+    void Set( bool value ) {
+	return m_pEIO->SetDigitalOutput(m_channel, value);
+    }
+};
+
 class OI {
 private:
-    Joystick* m_stick;
-    JoystickButton* m_gamepadButtonA;
-    JoystickButton* m_gamepadButtonB;
-    JoystickButton* m_gamepadButtonX;
-    JoystickButton* m_gamepadButtonY;
-    JoystickButton* m_gamepadLeftBumper;
-    JoystickButton* m_gamepadRightBumper;
-    JoystickButton* m_gamepadBack;
-    JoystickButton* m_gamepadStart;
+    DriverStation *m_pDS;
+    DriverStationEnhancedIO *m_pEIO;
+    DriverStationLCD *m_pLCD;
+
+    Joystick* m_pStick;
+    JoystickButton* m_pGamepadButtonA;
+    JoystickButton* m_pGamepadButtonB;
+    JoystickButton* m_pGamepadButtonX;
+    JoystickButton* m_pGamepadButtonY;
+    JoystickButton* m_pGamepadLeftBumper;
+    JoystickButton* m_pGamepadRightBumper;
+    JoystickButton* m_pGamepadBack;
+    JoystickButton* m_pGamepadStart;
+
+    DSAnalogInput* m_pClimber;
+    DSAnalogInput* m_pTip;
+    DSAnalogInput* m_pSpeedAdjust;
+    DSAnalogInput* m_pShooterPosition;
+
+    DSDigitalInput* m_pDump;
+    DSDigitalInput* m_pCameraLight;
+    DSDigitalInput* m_pCameraPosition;
+    DSDigitalInput* m_pQueryButton;
+    DSDigitalInput* m_pSpeedOverride;
+    DSDigitalInput* m_pLaunch;
+    DSDigitalInput* m_pKey;
+
+    DSDigitalOutput* m_pReadyLED;
 
     // aiming
-    Rotate* m_rotateFwd;
-    Rotate* m_rotateRev;
+    Rotate* m_pRotateFwd;
+    Rotate* m_pRotateRev;
 
     // shooting
-    ShootCommand* m_shootShort;
-    ShootCommand* m_shootMid;
-    ShootCommand* m_shootLong;
+    ShootCommand* m_pShootShort;
+    ShootCommand* m_pShootMid;
+    ShootCommand* m_pShootLong;
 
 public:
     OI();
     void Initialize();
-    Joystick* getStick() { return m_stick; }
+    DriverStation *getDS() { return m_pDS; }
+    Joystick* getStick() { return m_pStick; }
+    DriverStationEnhancedIO* getEIO() { return m_pEIO; }
+    DriverStationLCD* getLCD() { return m_pLCD; }
 };
 
 #endif
